ba483f4c5e131f24a294ff6120341c895840aa53
PJC-1 <jumpzine@gmail.com>
2018-03-21 21:38:23 -0700

    added fontawesome state for the calendar and some functions to format date

--- src/Components/Calendar/index.js
+++ src/Components/Calendar/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
 import moment from 'moment';
+import './calendar.css';
 
 export default class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.width = props.width || '350px';
+    this.style = props.style || {};
+  }
+
+  state = {
+    dateContext: moment(),
+    today: moment(),
+    showMonthPopup: false,
+    showYearPopup: false
+  }
+
+  weekdays = moment.weekdays();
+  weekdaysShort = moment.weekdaysShort();
+  months = moment.months();
+
+  year = () => {
+    return this.state.dateContext.format("Y");
+  }
+  month = () => {
+    return this.state.dateContext.format("MMMM");
+  }
+  daysInMonth = () => {
+    return this.state.dateContext.daysInMonth();
+  }
+  currentDate = () => {
+    return this.state.dateContext.get("date");
+  }
+  currentDay = () => {
+    return this.state.dateContext.format("D");
+  }
+
   render() {
     return (
       <div className="calendar-container">
